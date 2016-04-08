6c9ebedc6fa4cd2de6afa53e52a4b2a2397d1875
Jackon <jiekunyang@gmail.com>
2016-04-08 23:31:32 +0800

    count hits by redis incr in home page

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,7 +2,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from django_redis import get_redis_connection
+
+
+redis = get_redis_connection("default")
+
 
 @api_view(['GET'])
 def home(request):
-    return Response('hello, django startup')
+    key = 'test:hits'
+    redis.incr(key)
+    return Response('Hello World! I have been seen %s times.' % redis.get(key))
