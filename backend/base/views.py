4b6ee9a3f142c79814d7d01a67cabef8bf8ba7f9
Jackon <jiekunyang@gmail.com>
2016-04-09 16:20:26 +0800

    show host name in default home page. used for nginx bl settings.

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,15 +1,19 @@
 # -*- coding:utf-8 -*-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import socket
 
 from django_redis import get_redis_connection
 
 
 redis = get_redis_connection("default")
+host = socket.gethostname()
 
 
 @api_view(['GET'])
 def home(request):
     key = 'test:hits'
     redis.incr(key)
-    return Response('Hello World! I have been seen %s times.' % redis.get(key))
+    return Response('Hello World!'
+                    'I have been seen %s times.'
+                    'My Host name is %s.' % (redis.get(key), host))
