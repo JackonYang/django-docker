446dd90a2a3e037e90ee5766928c6bbdae027d86
Jackon <jiekunyang@gmail.com>
2016-05-10 08:52:40 +0800

    build frontend by webpack.

---
+++ frontend/webpack.config.js
@@ -0,0 +1,29 @@
+/**
+ * webpack Config
+ * 配置独立项、入口文件、输出项信息
+ */
+var webpack = require('webpack');
+var path = require('path');
+var node_modules_dir = path.join(__dirname, 'node_modules');
+var components_dir = path.join(__dirname, 'components');
+var views_dir = path.join(__dirname,'views');
+
+
+config = {
+  entry: path.join(views_dir, 'home/home.js'),
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file-loader?name=templates/[name].[ext]'
+      },
+    ]
+  }
+};
+
+
+module.exports = config;
