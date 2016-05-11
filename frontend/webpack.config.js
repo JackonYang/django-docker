085b665a246eb773c6b53c343a3064cc84779e1f
Jackon <jiekunyang@gmail.com>
2016-05-11 23:34:03 +0800

    multi entrys

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -3,18 +3,24 @@
  * 配置独立项、入口文件、输出项信息
  */
 var webpack = require('webpack');
+var fs = require('fs');  // file system
 var path = require('path');
 var node_modules_dir = path.join(__dirname, 'node_modules');
 var components_dir = path.join(__dirname, 'components');
 var views_dir = path.join(__dirname,'views');
 var output_dir = path.join(__dirname, '../backend');
 
+var entrys = {};
+fs.readdirSync(views_dir).forEach(function (name, idx) {
+    entrys[name] = path.join(views_dir, name, name+'.js')
+});
+
 
 config = {
-  entry: path.join(views_dir, 'home/home.js'),
+  entry: entrys,
   output: {
     path: output_dir,
-    filename: "static/bundle.js"
+    filename: "static/[name].js"
   },
   module: {
     loaders: [
